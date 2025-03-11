46b753383892daac67e555896661492544df8b5e
Renan Freitas <120645756+RFreitasAnjos@users.noreply.github.com>
2025-03-10 21:59:29 -0300

    Second Commit

---
+++ src/test/java/me/dio/renanfreitas/avanade_decola/AvanadeDecolaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.renanfreitas.avanade_decola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvanadeDecolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
